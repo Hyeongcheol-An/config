932c523f316de5864562d575b4674b4d934f3d8f
Hyeongcheol An <anh1026@kaist.ac.kr>
2021-04-29 04:45:33 +0000

    Add README.md file

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 sudo apt-get install build-essential libssl-dev
 
 git config --global core.editor vim
+git config --global user.email "anh1026@kaist.ac.kr"
+git config --global user.name "Hyeongcheol An"
+
+sudo apt install python3
+sudo apt install python3-pip
